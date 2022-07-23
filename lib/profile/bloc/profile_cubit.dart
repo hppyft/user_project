bcd7e52cc7d9c068b9193809ea73b6cb11c2a9df
Léo HppyFt Soares <leohppyftsoares@gmail.com>
2022-07-23 18:06:26 -0300

    profile - add connectivity verification

--- lib/profile/bloc/profile_cubit.dart
+++ lib/profile/bloc/profile_cubit.dart
@@ -1,27 +1,40 @@
 import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:user_project/profile/bloc/profile_state.dart';
 import 'package:user_project/profile/repository/profile_model.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
   ProfileCubit() : super(const LoadingProfile());
 
-  Future<void> loadProfile() async {
-    await Future.delayed(Duration(seconds: 2));
-    // try {
-    //   final ProfileModel profile = ProfileModel(
-    //       name: 'Leonardo Soares',
-    //       email: 'leohppyftsoares@gmail.com',
-    //       avatarUrl: 'https://randomuser.me/api/portraits/thumb/women/60.jpg');
-    //   emit(ProfileLoaded(profile: profile));
-    // } catch (e) {
-    emit(const ErrorLoadingProfile(
-        message:
-            'Ocorreu um erro ao tentar resgatar o perfil, por favor tente novamente.'));
-    // }
-  }
-
   Future<void> reloadProfile() async {
     emit(const LoadingProfile());
     await loadProfile();
   }
+
+  Future<void> loadProfile() async {
+    try {
+      bool internetConnection = await hasInternet();
+      if(!internetConnection){
+        emit(const ErrorLoadingProfile(
+            message:
+            'Parece que você não está conectado a internet, por favor conecte-se e tente novamente.'));
+      } else {
+        final ProfileModel profile = ProfileModel(
+            name: 'Leonardo Soares',
+            email: 'leohppyftsoares@gmail.com',
+            avatarUrl: 'https://randomuser.me/api/portraits/thumb/women/60.jpg');
+        emit(ProfileLoaded(profile: profile));
+      }
+    } catch (e) {
+    emit(const ErrorLoadingProfile(
+        message:
+            'Ocorreu um erro ao tentar resgatar o perfil, por favor tente novamente.'));
+    }
+  }
+
+  Future<bool> hasInternet() async{
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    return connectivityResult != ConnectivityResult.none;
+  }
+
 }
